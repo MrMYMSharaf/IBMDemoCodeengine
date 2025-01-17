02ee8ece72a78b1784a0dc9ace977acf3960aea4
MrMYMSharaf <mymsharaff@gmail.com>
2025-01-17 17:07:40 +0530

    check workin header port02

--- src/app.py
+++ src/app.py
@@ -2,5 +2,6 @@ import streamlit as st
 
 st.title("Code Engin + Docker")
 st.header("header")
+st.write("it's working in docker 01")
 st.write("it's working in docker")
 st.write("it's working in code engine")
